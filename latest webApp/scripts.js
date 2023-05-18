17bb4420620830491283abcb231f8bcdf1d3f189
HishamAbosena <Habosena18@icloud.com>
2023-05-18 16:36:45 +0300

    another project

---
+++ latest webApp/scripts.js
@@ -0,0 +1,38 @@
+// const loginUserName = document.getElementById("submit");
+// const userNameGetter = document.getElementById("name");
+// const passwordGetter = document.getElementById("Password");
+// const userAlertMessage = document.getElementById("nameFady");
+// const passwordAlertMessage = document.getElementById("passwordFady");
+// const userName = "admin";
+// const checkPassword = "admin";
+// console.log(userNameGetter.getAttribute("placeholder"))
+// const required = (fun) => {
+//     // console.log(fun)
+//     // debugger
+//     fun.classList.add("is-invalid");
+//     fun.getAttribute("placeholder") == "Username" ? userAlertMessage.innerText = "You should fill this field" : "";
+//     fun.getAttribute("placeholder") == "Password" ? passwordAlertMessage.innerText = "You should fill this field" : "";
+// }
+// const isValid=(fun)=>{
+//     fun.classList.add("is-valid");  
+// }
+// // console.log(userNameGetter,"\n",passwordGetter)
+// function validity() {
+    
+//     userAlertMessage.innerText="";
+//     passwordAlertMessage.innerText="";
+//     userNameGetter.classList.remove("is-invalid");
+//     passwordGetter.classList.remove("is-invalid");
+//     let name = userNameGetter.value;
+//     let password = passwordGetter.value;
+//     name = name.trim();
+//     console.log(name);
+//     password = password.trim();
+//     name === "" ? required(userNameGetter) : isValid(userNameGetter);
+//     password == "" ? required(passwordGetter) : isValid(passwordGetter);
+//     // console.log(name)
+//     // console.log(password)
+// }
+// loginUserName.addEventListener('click', validity);
+
+
